2693275d9e58f7f2634e75939523e31d4db6a68c
Fardin Kashef <fardinkashef1397@gmail.com>
2025-07-17 10:36:59 +0330

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ToastProvider from "@/lib/providers/toast-provider";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <ToastProvider />
         {children}
+        <GoogleAnalytics gaId="G-TYGGMHZJHF" />
       </body>
     </html>
   );
